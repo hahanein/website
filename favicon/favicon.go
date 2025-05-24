8ca81cbe2dcd733934078250362616f2a76341ba
Benjamin Westphal <hahanein@users.noreply.github.com>
2025-05-24 19:57:06 +0200

    Revert "randomize favicon"
    This reverts commit 1de52c11d72c019e1c00f7a5a21a4f2e379faf0d.

--- favicon/favicon.go
+++ favicon/favicon.go
@@ -3,16 +3,12 @@ package favicon
 import (
 	"encoding/binary"
 	"log"
-	"math/rand"
 	"net/http"
-	"time"
 )
 
-var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+var ico = make([]byte, 6+16+40+1024)
 
-func createBaseICO() []byte {
-	ico := make([]byte, 6+16+40+1024)
-
+func init() {
 	// ICO header
 	binary.LittleEndian.PutUint16(ico[0:2], 0) // Reserved
 	binary.LittleEndian.PutUint16(ico[2:4], 1) // Type: ICO
@@ -35,46 +31,17 @@ func createBaseICO() []byte {
 	binary.LittleEndian.PutUint16(ico[34:36], 1)  // Planes
 	binary.LittleEndian.PutUint16(ico[36:38], 32) // Bits per pixel
 
-	// Draw main square
-	for y := range 16 {
-		for x := range 16 {
-			if x >= 4 && x < 12 && y >= 4 && y < 12 {
-				offset := 62 + ((15-y)*16+x)*4
-				ico[offset] = 0x0a   // Blue
-				ico[offset+1] = 0x0a // Green
-				ico[offset+2] = 0x0a // Red
-				ico[offset+3] = 0xff // Alpha
-			}
-		}
+	// Fill with color #0a0a0a (BGR format with alpha)
+	for i := 62; i < 62+1024; i += 4 {
+		ico[i] = 0x0a   // Blue
+		ico[i+1] = 0x0a // Green
+		ico[i+2] = 0x0a // Red
+		ico[i+3] = 0xff // Alpha
 	}
-
-	return ico
 }
 
 func Handler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "image/x-icon")
-
-	ico := createBaseICO()
-
-	// Random 2x2 square position within the main square (6x6 area)
-	startX := 4 + rng.Intn(6)
-	startY := 4 + rng.Intn(6)
-
-	// Random alpha: either fully opaque or transparent
-	alpha := byte(0)
-	if rng.Intn(2) == 1 {
-		alpha = 0xff
-	}
-
-	// Draw 2x2 square
-	for dy := 0; dy < 2 && startY+dy < 12; dy++ {
-		for dx := 0; dx < 2 && startX+dx < 12; dx++ {
-			y := startY + dy
-			x := startX + dx
-			offset := 62 + ((15-y)*16+x)*4
-			ico[offset+3] = alpha
-		}
-	}
 
 	if _, err := w.Write(ico); err != nil {
 		log.Printf("Error writing favicon: %v", err)
