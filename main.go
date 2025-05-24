5129ac4a514fd8e6df544d46bb60768e88adfd93
Benjamin Westphal <hahanein@users.noreply.github.com>
2025-05-24 18:58:10 +0200

    make port configurable

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	port := flag.String("port", "", "port to listen on (required)")
+	flag.Parse()
+
+	if *port == "" {
+		log.Fatal("port flag is required")
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		if _, err := fmt.Fprintln(w, "Hello World"); err != nil {
 			log.Printf("Error writing response: %v", err)
 		}
 	})
 
-	log.Println("listening on :8767")
-	log.Fatal(http.ListenAndServe(":8767", nil))
+	log.Printf("listening on :%s", *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
